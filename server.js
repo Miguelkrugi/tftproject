3853e1535931e8d9f344f0ca4d4c396619933775
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-10-12 01:31:17 +0100

    ch35

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Server is Up!'))
+app.get('/', (req, res) => res.send('Server is Upp!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
