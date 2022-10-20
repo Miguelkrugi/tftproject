663e623a1754a7250f6d01650d64a1379ea51a94
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-10-20 11:11:50 +0100

    ch152

--- server.js
+++ server.js
@@ -6,14 +6,29 @@ app.get('/users', (req, res) => res.send('Ola Mundo!'))
 
 app.listen(3000, () => console.log('Node.js app listening on port 3000.'))
 */
+/*
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));*/
 
 const express = require('express')
-var cors = require("cors");  //ADICIONADO
+const cors = require('cors');
+
+const corsOptions = {
+   origin: 'http://localhost:3000',
+   credentials: true,
+   optionSucessStatus: 200
+}
 const app = express()
 const port = 8000
 
-app.use(cors()); //ADICIONADO
+
 app.get('/', (req, res) => res.send('Server is Upp!'))
 
+app.use(cors(corsOptions));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
