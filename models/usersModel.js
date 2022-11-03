b84d7d6fb2ada0fd6b0e3d376b4c9ee721d6c5c8
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-11-03 12:58:46 +0000

    ch36

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 var pool = require("./connection");
-var brcypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 var salt = 10;
 
 module.exports.getUsers = async function() {
@@ -32,15 +32,11 @@ module.exports.updatePontosUtilizador = async function(uti_id, pontossobrantes){
 }
 
 module.exports.saveUser = async function(user) {
-    //console.log("[usersModel.saveUser] user = " + JSON.stringify(user));
-     //checks all fields needed and ignores other fields
-    /*if (typeof user != "object" || failUser(user)) {
-        if (user.errMsg)
-            return { status: 400, data: { msg: user.errMsg } };
-        else
-            return { status: 400, data: { msg: "Malformed data" } };
-    }
-    let password = brcypt.hashSync(user.user_password, salt);*/
+   
+    let password = bcrypt.hashSync(user.utilizador_password, salt);
+
+    console.log("Hash: " + password);
+
     try {
 
         let sql =
@@ -51,7 +47,7 @@ module.exports.saveUser = async function(user) {
             "RETURNING utilizador_id";
 
             //console.log(user.user_name + "|" + user.user_password + "|" + user.user_morada + "|" + user.user_email + "|" + user.user_points + "|" + user.user_admin + "|" + user.user_pt + "|" + user.user_nutri);
-        let result = await pool.query(sql, [user.utilizador_name, user.utilizador_password, user.utilizador_email, user.utilizador_dinheiro, user.utilizador_pass]);
+        let result = await pool.query(sql, [user.utilizador_name, password, user.utilizador_email, user.utilizador_dinheiro, user.utilizador_pass]);
         
         return { status: 200, result: result };
     } catch (err) {
@@ -126,10 +122,18 @@ module.exports.authUser = async function(uti_name){
 
         let passwordb = result.rows[0].utilizador_password;
 
+        let insertedpassword = bcrypt.hashSync(uti_name.utilizador_password, salt);
+        console.log("INSERTED: " + insertedpassword);
         console.log("authUser.passwordb = " + JSON.stringify(passwordb));
         console.log("authUser.uti_name.user_password = " + JSON.stringify(uti_name.utilizador_password));
 
-        let valor = (uti_name.utilizador_password == passwordb);
+        console.log("TEST PASSWORD: " + ins);
+
+        console.log("PASSWORD: " + passwordb);
+
+        let valor = bcrypt.compareSync(uti_name.utilizador_password == passwordb);
+
+      //  let valor = (uti_name.utilizador_password == passwordb);
 
         console.log("authUser.valor = " + JSON.stringify(valor));
 
